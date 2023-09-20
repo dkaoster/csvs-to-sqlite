c21c53ebeb938d5719458367c4da953e93c08fa0
chrismp <christopher.mark.persaud@gmail.com>
2023-09-20 17:10:45 -0400

    fixed how tuple `fts` is formed

--- csvs_update_sqlite/cli.py
+++ csvs_update_sqlite/cli.py
@@ -304,9 +304,10 @@ def cli(
         for table, df in created_tables.items():
             if fts_regex:
                 for col in df:
+                    print(fts_regex) 
+                    print(col)
                     if re.match(fts_regex, col, re.IGNORECASE):
-                        fts = (*fts,col) if fts else col
-            
+                        fts = (*fts,col,) if fts else (col,)_
             if fts:
                 for fts_column in fts:
                     if fts_column not in df.columns:
